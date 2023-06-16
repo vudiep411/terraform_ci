b3eb94025be36e641d6d22505af52bd495a7f520
Vu Diep <54611122+vudiep411@users.noreply.github.com>
2023-06-16 14:44:18 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.27.0"
+    }
+  }
+}
+   
+
+# Configure the AWS Provider
+provider "aws" {
+    region = "us-west-1"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+# Create Key-pair
+# resource "aws_key_pair" "tf-key-pair" {
+#   key_name = var.key_pair_name
+#   public_key = tls_private_key.rsa-key.public_key_openssh
+# }
+# resource "tls_private_key" "rsa-key" {
+#   algorithm = "RSA"
+#   rsa_bits  = 4096
+# }
+# resource "local_file" "tf-key" {
+#   content  = tls_private_key.rsa-key.private_key_pem
+#   filename = "${var.key_pair_name}.pem"
+# }
+
+# Aws instance
+resource "aws_instance" "my-tf-server" {
+  ami = "ami-0bd4d695347c0ef88"
+  # key_name = aws_key_pair.tf-key-pair.key_name
+  instance_type = "t2.micro"
+  tags = {
+    Name= "my-server1"
+  }
+}
+
+resource "aws_vpc" "my-server-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = aws_vpc.my-server-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    name = "prod-subnet"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" { 
+  type = string 
+} 
+variable "secret_key" { 
+  type = string 
+}
+variable "key_pair_name" {
+    type = string 
+}
